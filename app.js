48ca744ef7e00a11ee6adb8ac8e49a169041c62e
weiwei0223 <1215566538@qq.com>
2017-06-02 14:34:07 +0800

    添加定位

--- app.js
+++ app.js
@@ -6,15 +6,52 @@ function Map(id){
     this.map = new BMap.Map(id);
     //设置地图的中心点和缩放比例 初始化地图
     this.map.centerAndZoom("北京");
-}
-Map.prototype.addPanorama = function () {
-
+}prototype.addPanorama = function () {
     var panorama = new BMap.PanoramaControl();
 
     this.map.addControl(panorama);
 };
 
-//初始化应用程序的方法
+//定位
+Map.prototype.currentPosition = function () {
+
+    return new Promise(function (success) {
+
+        var loc = new BMap.Geolocation();
+
+        //获得当前的位置信息
+        loc.getCurrentPosition(function (result) {
+
+            //    result 获得到的定位之后的结果集
+
+            success(result);
+        });
+
+    });
+
+};
+//更新用户位置信息
+Map.prototype.updateUserLocation = function () {
+    //result 定位到的位置信息
+    //then这个函数   当success被调用的时候  then里面的函数 立刻调用
+    this.currentPosition().then(function (result) {
+
+        //创建 用户显示位置的  视图
+        var marker = new BMap.Marker(result.point);
+
+        //添加到地图上
+        this.map.addOverlay(marker);
+
+        //重新设置地图的 心点
+        this.map.setCenter(result.point);
+
+        //让地图 移动到 中心点的位置
+        this.map.panTo(result.point);
+
+    }.bind(this));
+};
+
+//始化应用程序的方法
 function init() {
     var map = new Map("mapContainer");
     map.addPanorama();
